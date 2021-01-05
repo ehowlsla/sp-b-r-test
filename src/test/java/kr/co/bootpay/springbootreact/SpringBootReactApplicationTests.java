1fc013b825dd3d80b49c1b4ffc6a53aa879d6cf1
ehowlsla <rupy1014@gmail.com>
2021-01-05 11:45:21 +0900

    first commit

---
+++ src/test/java/kr/co/bootpay/springbootreact/SpringBootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.bootpay.springbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
