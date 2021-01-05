e81ff894e9f447e3645a61c12862ea9c1a0d7414
ehowlsla <rupy1014@gmail.com>
2021-01-05 13:19:09 +0900

    success

--- src/main/java/kr/co/bootpay/springbootreact/controller/HelloController.java
+++ src/main/java/kr/co/bootpay/springbootreact/controller/HelloController.java
@@ -1,2 +1,14 @@
-package kr.co.bootpay.springbootreact.controller;public class helloController {
+package kr.co.bootpay.springbootreact.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+//    @GetMa
+    @GetMapping("/hello")
+    public @ResponseBody String index() {
+        return "hello";
+    }
 }
